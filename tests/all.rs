6cc745a5166fc71bc1668f321a6d6cba3396a586
Victor-Nicolae Savu <victor.nicolae.savu@gmail.com>
2017-07-02 21:30:28 +0200

    Test the return of `all`

--- tests/all.rs
+++ tests/all.rs
@@ -3,16 +3,18 @@ extern crate o3;
 
 use o3::iter::wrap::Wrap;
 use o3::comb::all::All;
+use o3::map::ret::MapReturn;
 
 #[test]
 fn all() {
-    let first = (0..5).wrap();
-    let second = (0..10).wrap();
+    let first = (0..5).wrap().map_return(|_| "first");
+    let second = (0..10).wrap().map_return(|_| "second");
 
     let mut trace = vec![];
-    each!(first.all(second) => i in {
+    let both = each!(first.all(second) => i in {
         trace.push(i);
     });
 
     assert_eq!(trace, [0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 6, 7, 8, 9]);
+    assert_eq!(both, ("first", "second"));
 }
