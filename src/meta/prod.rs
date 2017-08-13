a1cea3bac8ffd11c736832261098f4fffa07d4f3
Victor-Nicolae Savu <victor.nicolae.savu@gmail.com>
2017-08-14 00:13:49 +0200

    Remove unnecessary scruff in meta::prod

--- src/meta/prod.rs
+++ src/meta/prod.rs
@@ -1,57 +1,17 @@
-
 pub trait Prod {
-    type Head;
-    type Tail: Prod;
+    type Left;
+    type Right;
 
-    fn split(self) -> (Self::Head, Self::Tail);
+    fn to_canonical(self) -> (Self::Left, Self::Right);
 }
 
-impl Prod for ! {
-    type Head = !;
-    type Tail = !;
+impl<A, B> Prod for (A, B) {
+    type Left = A;
+    type Right = B;
 
-    fn split(self) -> (Self::Head, Self::Tail) {
-        unreachable!()
-    }
-}
-
-impl Prod for () {
-    type Head = ();
-    type Tail = ();
-
-    fn split(self) -> (Self::Head, Self::Tail) {
-        ((), ())
-    }
-}
-
-impl<A> Prod for (A,) {
-    type Head = A;
-    type Tail = ();
-
-    fn split(self) -> (Self::Head, Self::Tail) {
-        (self.0, ())
-    }
-}
-
-impl<A, B> Prod for (A, B)
-    where B: Prod
-{
-    type Head = A;
-    type Tail = B;
-
-    fn split(self) -> (Self::Head, Self::Tail) {
+    fn to_canonical(self) -> (Self::Left, Self::Right) {
         self
     }
-}
-
-pub trait Pair<H, T> {
-    type ProdTail: Prod<Head = T, Tail = ()>;
-    type Output: Prod<Head = H, Tail = Self::ProdTail>;
-}
-
-impl<H, T> Pair<H, T> for (H, T) {
-    type ProdTail = (T,);
-    type Output = (H, Self::ProdTail);
 }
 
 
@@ -63,7 +23,7 @@ mod tests {
         use super::Prod;
 
         let x = (1, ("Hello", ()));
-        let (a, (b, ())) = x.split();
+        let (a, (b, ())) = x.to_canonical();
         assert_eq!(a, 1);
         assert_eq!(b, "Hello");
     }
